0ebb5167ea1aaafb7cbb353ca7806d0386aa039b
br0keh <abertoemail+8@gmail.com>
2020-03-25 09:58:46 -0300

    fix: https required

--- src/App/services/api.js
+++ src/App/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://vinicius-consultas.herokuapp.com",
+    baseURL: "https://vinicius-consultas.herokuapp.com",
 });
 
 export default api;
